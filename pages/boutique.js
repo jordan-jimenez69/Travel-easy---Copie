f63dfa1a3cefa4cd50c1213acca6b0fe4dabaa97
Jordan JIMENEZ <jordan.jimenez@ecole-isitech.fr>
2024-07-26 08:11:59 +0200

    no loadung

--- pages/boutique.js
+++ pages/boutique.js
@@ -1,42 +1,28 @@
 import NewCategories from "@/composants/boutiques/NewCategories";
 import Footer from "@/composants/footer";
-import Loader from "@/composants/loader";
 import Navbar from "@/composants/navbar";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Categorie } from "@/models/categorie";
-import { useEffect, useState } from "react";
 
-export default function Boutique({ newCategories }) {
-  const [categories, setCategories] = useState(null);
-  const [loading, setLoading] = useState(true); // Ajout de l'état de chargement
-
-  useEffect(() => {
-    // Simuler un délai de chargement pour observer le loader
-    const timer = setTimeout(() => {
-      setCategories(newCategories);
-      setLoading(false); // Les données sont maintenant chargées
-    }, 500); // Ajustez le délai selon vos besoins
-
-    return () => clearTimeout(timer); // Nettoyer le timer au démontage
-  }, [newCategories]);
-
+export default function boutique({ newCategories }) {
+  console.log({ newCategories })
   return (
-    <>
-      <Navbar />
 
-      <div className="title-boutique">
-        <h1 className="title">Boutique</h1>
+    <>
+      <div>
+        <Navbar />
       </div>
 
-      <div className="content">
-        {loading ? (
-          <Loader /> // Affiche le loader pendant le chargement
-        ) : (
-          <NewCategories categories={categories} />
-        )}
+      <div className="title-boutique">
+        <h1 className="title">Boutique</h1> 
+      </div>
+
+      <div>
+        <NewCategories categories={newCategories} />
       </div>
 
       <Footer />
+
     </>
   );
 }
@@ -44,7 +30,6 @@ export default function Boutique({ newCategories }) {
 export async function getServerSideProps() {
   await mongooseConnect();
   const newCategories = await Categorie.find({}, null, { sort: { '_id': -1 } });
-
   return {
     props: {
       newCategories: JSON.parse(JSON.stringify(newCategories)),
