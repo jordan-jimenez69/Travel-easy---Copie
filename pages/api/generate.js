3165cd67bbb55c20995e1590159ef4e59ef19eb6
Jordan JIMENEZ <jordan.jimenez@ecole-isitech.fr>
2025-09-29 12:43:52 +0200

    update

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const apiKey = process.env.API_KEY;
 
   const genAI = new GoogleGenerativeAI(apiKey);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
   const prompt = "Donne moi une randonné a faire en france.";
   const result = await model.generateContent(prompt);
